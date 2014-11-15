ae1dac87af1915d297ca60bc8a72dc75aca336e7
Ian Nastajus <ian.nastajus@gmail.com>
2014-11-14 19:34:13 -0500

    login validates

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -4,10 +4,34 @@ class Main extends CI_Controller {
 
     function Login(){
         if($this->input->post()){
-            $this->load->view('header');
-            echo var_dump($this->input->post());
-            $this->load->view('loginpage');
-            $this->load->view('footer');
+            $this->load->library('form_validation');
+
+            $this->form_validation->set_rules('username', 'Username', 'trim|required|xss_clean');
+            $this->form_validation->set_rules('password', 'Password', 'required|xss_clean');
+
+            if ($this->form_validation->run() == FALSE)
+            {
+                //$this->load->view('myform');
+
+                //They did it wront send tem back to login
+                $this->load->view('header');
+                echo var_dump($this->input->post());
+                echo validation_errors();
+                $this->load->view('loginpage');
+                $this->load->view('footer');
+            }
+            else
+            {
+                //$this->load->view('formsuccess');
+                //Ther wew right send tem to the members page
+                $this->load->view('header');
+                echo var_dump($this->input->post());
+                echo "Great success!";
+                $this->load->view('loginpage');
+                $this->load->view('footer');
+            }
+
+
         }else{
             $this->load->view('header');
             $this->load->view('loginpage');
