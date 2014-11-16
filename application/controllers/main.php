ddc3700d51c47f58b031f40ed9f904dab341aca7
Ian Nastajus <ian.nastajus@gmail.com>
2014-11-15 19:12:45 -0500

    tried submitting hashed salt, still wrong password

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -3,30 +3,34 @@
 class Main extends CI_Controller {
 
     function Login(){
+
+        $passwordSalty = hash('sha512',"stuff"."please");
+        echo "stuff please<br>";
+        echo $passwordSalty."<br>";
+
         if($this->input->post()){
             $this->load->library('form_validation');
 
             $this->form_validation->set_rules('username', 'Username', 'trim|required|xss_clean');
             $this->form_validation->set_rules('password', 'Password', 'required|xss_clean');
 
-
-
             if ($this->form_validation->run() == TRUE)
             {
 
                 $username = $this->input->post('username');
                 $password = $this->input->post('password');
+
                 $this->load->model('users');
 
-                $cond = $this->users->Login($username, $password);
+                $cond = $this->users->Login($username, $passwordSalty );
 
                 if ($cond){
 
                     //$this->load->view('formsuccess');
                     //They were right send them to the members page
                     $this->load->view('header');
-                    echo var_dump($this->input->post());
-                    echo validation_errors();
+                    //echo var_dump($this->input->post());
+                    //echo validation_errors();
                     $this->load->view('successpage');
                     //$this->load->view('footer');
 
@@ -35,8 +39,8 @@ class Main extends CI_Controller {
 
                     //They did it wrong send them back to login
                     $this->load->view('header');
-                    echo var_dump($this->input->post());
-                    echo validation_errors();
+                    //echo var_dump($this->input->post());
+                    //echo validation_errors();
                     echo "<br>Wrong username or password<br>";
                     $this->load->view('loginpage');
                     //$this->load->view('footer');
@@ -51,8 +55,8 @@ class Main extends CI_Controller {
 
                 //They did it wrong send them back to login
                 $this->load->view('header');
-                echo var_dump($this->input->post());
-                echo validation_errors();
+                //echo var_dump($this->input->post());
+                //echo validation_errors();
                 echo "<br>Invalid input<br>";
                 $this->load->view('loginpage');
                 //$this->load->view('footer');
