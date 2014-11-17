7e89d46aa44ddc07e63029ae22f16604baba368b
Victor <victor@coinclutch.com>
2014-11-16 22:12:22 -0500

    Added CDN references to Jquery, Jquery UI and Bootstrap 3

--- application/views/Header.php
+++ application/views/Header.php
@@ -1,7 +1,9 @@
 <!-- header -->
 <html>
     <head>
-        <title></title>
+        <title>Incrementor Game</title>
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
     </head>
     <body>
     HEADER!!!!<br>
