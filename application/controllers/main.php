8cb767c1cc5b5bde90efe37bdbb3993920c39323
Victor <victor.nguyen@hotmail.com>
2014-11-17 15:46:41 -0500

    Prototyped users login

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -1,11 +1,27 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Main extends CI_Controller {
+     public function loginCheck() {
+        if ($this->session->userdata('logged_in')) {
+            $this->load->model("users");
+            $data['session_data'] = $this->session->userdata('logged_in');
+            $data['username'] = $data['session_data']['username'];
+
+            
+        } else {
+            //If no session, redirect to login page
+            header('location: home');
+        }
+    }
     //Home Page
     function index(){
         $this->load->view('header');
         $this->load->view('home');
         $this->load->view('footer');
+    }
+    
+    function home(){
+        $this->index();
     }
 
     function Login(){
@@ -25,28 +41,25 @@ class Main extends CI_Controller {
                 $this->load->model('users');
 
                 $cond = $this->users->Login($username, $password );
-
+                echo var_dump($cond);
                 if ($cond){
-
-                    //$this->load->view('formsuccess');
-                    //They were right send them to the members page
-                    $this->load->view('header');
-                    //echo var_dump($this->input->post());
-                    //echo validation_errors();
-                    $this->load->view('successpage');
-                    $this->load->view('footer');
+                    $sess_array = array();
+                    foreach($cond as $row)
+                    {
+                      $sess_array = array(
+                        'id' => $row->id,
+                        'username' => $row->username
+                      );
+                      $this->session->set_userdata('logged_in', $sess_array);
+                    }
+                    header('location: members');
 
                 }
                 else {
-
-                    //They did it wrong send them back to login
                     $this->load->view('header');
-                    //echo var_dump($this->input->post());
-                    //echo validation_errors();
                     echo "<br>Wrong username or password<br>";
                     $this->load->view('loginpage');
                     $this->load->view('footer');
-
                 }
 
 
@@ -75,6 +88,13 @@ class Main extends CI_Controller {
     function Register(){
         $this->load->view('header');
         $this->load->view('register');
+        $this->load->view('footer');
+    }
+    
+    function Members(){
+        $data = $this->loginCheck();
+        $this->load->view('header');
+        $this->load->view('successpage');
         $this->load->view('footer');
     }
 
--- application/views/LoginPage.php
+++ application/views/LoginPage.php
@@ -1,5 +1,16 @@
-<form method="POST" action="#">
-    <input type="text" name="username"/>
-    <input type="text" name="password"/>
-    <input type="submit" value="Submit"/>
-</form>
+<div class="row">
+    <div class="col-xs-12">
+         <form role="form" method="POST" action="#">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="exampleInputEmail1" name="username" placeholder="Enter Username">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="Password">
+            </div>
+
+            <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+        </div>
+</div>
