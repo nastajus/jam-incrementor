035f569a5772346eeb6ff0b6a56da60d704070b4
Ian Nastajus <ian.nastajus@gmail.com>
2014-11-17 18:34:11 -0500

    removed unnecessary underscores. moved to main

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -84,6 +84,85 @@ class Main extends CI_Controller {
             $this->load->view('footer');
         }
     }
+
+    //move this function to main.php controller when complete.
+    public function Register(){
+
+        if($this->input->post()){
+
+            $this->load->library('form_validation');
+
+            $this->form_validation->set_rules('username', 'Username', 'trim|required|xss_clean|callback_space_check|min_length[3]|max_length[32]|alpha_numeric|is_unique[users.username]');
+            $this->form_validation->set_rules('password', 'Password', 'required|xss_clean|matches[passconf]|min_length[8]');
+            $this->form_validation->set_rules('passconf', 'Password Confirmation', 'required|xss_clean');
+            $this->form_validation->set_rules('email', 'Email', 'required|valid_email|is_unique[users.email]|callback__space_check|xss_clean');
+            $this->form_validation->set_rules('agree', 'I agree to terms and services.', 'callback_accept_terms' );
+
+            if ($this->form_validation->run()){
+                $username = $this->input->post('username');
+                $password = $this->input->post('password');
+                $email = $this->input->post('email');
+
+                $this->load->model('users');
+                $this->load->library('format');
+
+                $cond = $this->users->Insert(Format::User($username, $password, $email));
+
+                var_dump($cond);
+
+                if ($cond){
+                    $data['message']= "Successfully Registered!!!";
+                    $this->load->view('header');
+                    $this->load->view('Message',$data);
+                    $this->load->view('footer');
+                }
+                else {
+                    $data['message']= "Error Registering" . "<br/>" . $this->form_validation->error_string();
+                    $this->load->view('header');
+                    $this->load->view('registerpage',$data);
+                    $this->load->view('footer');
+                }
+
+            }else {
+                $data['message']= "Error Registering" . "<br/>" . $this->form_validation->error_string();
+                $this->load->view('header');
+                $this->load->view('registerpage',$data);
+                $this->load->view('footer');
+            }
+
+        }
+        else {
+            $this->load->view('header');
+            $this->load->view('registerpage');
+            $this->load->view('footer');
+
+        }
+    }
+
+    function space_check($str)
+    {
+
+        $pattern = '/ /';
+        $result = preg_match($pattern, $str);
+
+        if ($result)
+        {
+            $this->form_validation->set_message('space_check', 'The %s field can not have spaces.');
+            return FALSE;
+        }
+        else
+        {
+            return TRUE;
+        }
+    }
+
+    function accept_terms(){
+        if (isset($_POST['accept'])) return TRUE;
+        $this->form_validation->set_message('accept_terms', 'Please read and accept our terms and conditions.');
+        return FALSE;
+
+    }
+
     function Contact(){
         $this->load->view('header');
         $this->load->view('contact');
@@ -99,12 +178,7 @@ class Main extends CI_Controller {
         $this->load->view('faq');
         $this->load->view('footer');
     }
-    function Register(){
-        $this->load->view('header');
-        $this->load->view('register');
-        $this->load->view('footer');
-    }
-    
+
   
 
 }
