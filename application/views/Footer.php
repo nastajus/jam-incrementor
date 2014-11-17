236ba9546cb5748769a18559ecb671f825508184
Victor <victor@coinclutch.com>
2014-11-16 22:13:15 -0500

    Missed line on footer, added Home View =P

--- application/views/Footer.php
+++ application/views/Footer.php
@@ -3,5 +3,6 @@ FOOTER!!!
 <!-- footer -->
 </body>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </html>
